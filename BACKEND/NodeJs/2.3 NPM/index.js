5483b982bf55060fd6dad757cbf782e2ec48186c
Azizul Islam <azizulislam5360@gmail.com>
2025-10-30 12:03:45 +0600

    Add files via upload

---
+++ BACKEND/NodeJs/2.3 NPM/index.js
@@ -0,0 +1,19 @@
+
+
+
+/*
+        ----Sillyname----
+
+import generateName from "sillyname"
+var sillyName = generateName();
+
+console.log("My name is :"+sillyName+".");
+*/
+
+//         -------superheroes--------
+
+import {randomSuperhero} from 'superheroes';
+
+var superheroes=randomSuperhero();
+
+console.log("My name is : "+superheroes+".");
