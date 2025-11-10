6280f98a26968657b2ea3188ce4d3c4916dee862
Azizul Islam <azizulislam5360@gmail.com>
2025-11-10 19:05:29 +0600

    Set up Express server with home and about routes

--- BACKEND/Express With Node js/3.2 HTTP Request/index.js
+++ BACKEND/Express With Node js/3.2 HTTP Request/index.js
@@ -1,1 +1,17 @@
 
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res) => {
+    res.send("<h1>This is the Home Page</h1>");
+});
+
+app.get("/about",(req,res) => {
+    res.send("<h1>About Me</h1>");
+});
+
+app.listen(port,() => 
+{
+    console.log('Server running on Port : '+port+'.');
+})   
