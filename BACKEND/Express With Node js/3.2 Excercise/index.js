d82d2fa7fccac56418e3ec758e9c20099acf09f5
Azizul Islam <azizulislam5360@gmail.com>
2025-11-10 19:04:09 +0600

    Set up Express server with basic routes

--- BACKEND/Express With Node js/3.2 Excercise/index.js
+++ BACKEND/Express With Node js/3.2 Excercise/index.js
@@ -1,1 +1,22 @@
+import express from "express";
+const app = express();
+const port = 3000;
 
+app.get("/",(req,res) => {
+    res.send("<h1>This is the home Page</h1>");
+});
+
+app.get("/about",(req,res) => {
+    res.send("<h1>About Me</h1>");
+});
+
+//Allows Server to get contact ---- .get("/extension")
+app.get("/contact",(req,res) => {
+    res.send("<h1>Contact With Me.</h1>");
+});
+
+//Initialising Server-
+app.listen(port,() => 
+{
+    console.log('Server running on Port : '+port+'.');
+})   
