2b1d0688022a3e14c6ade5cee810304dd18a08b3
Azizul Islam <azizulislam5360@gmail.com>
2025-11-22 11:41:59 +0600

    Add basic React rendering for favorite foods

--- ReactJs/JSX Practise 1/src/index.js
+++ ReactJs/JSX Practise 1/src/index.js
@@ -1,1 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.querySelector("#root"));
+root.render(
+  <div>
+    <h1>My Favourite Foods-</h1>
+    <ul>
+      <li>Kacchi</li>
+      <li>Lemonade</li>
+      <li>And You🙈.</li>
+    </ul>
+  </div>
+);
 
