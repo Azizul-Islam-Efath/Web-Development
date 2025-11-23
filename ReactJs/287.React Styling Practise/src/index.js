9228db9a18db4765dcdc30ff3d116ba511df1343
Azizul Islam <azizulislam5360@gmail.com>
2025-11-24 00:33:53 +0600

    Implement dynamic greeting based on time of day

--- ReactJs/287.React Styling Practise/src/index.js
+++ ReactJs/287.React Styling Practise/src/index.js
@@ -1,1 +1,40 @@
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
 
+import react from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+const t = new Date();
+const Time = t.getHours();
+const Red = {
+  color: "red",
+};
+const Green = {
+  color: "green",
+};
+const Blue = {
+  color: "blue",
+};
+
+function greet() {
+  if (Time >= 0 && Time < 12) {
+    return <h1 style={Red}>Good Morning</h1>;
+  } else if (Time >= 12 && Time < 18) {
+    return <h1 style={Green}>Good Afternoon</h1>;
+  } else if (Time >= 18 && Time <= 24) {
+    return <h1 style={Blue}>Good Evening</h1>;
+  }
+}
+
+root.render(
+  <div>
+    <h1 className="heading">{greet()}</h1>
+  </div>
+);
