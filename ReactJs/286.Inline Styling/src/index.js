ccb8513c2b7224a55e5d018f94eac087e0b18601
Azizul Islam <azizulislam5360@gmail.com>
2025-11-23 16:15:21 +0600

    Add inline styling to Hello World header

--- ReactJs/286.Inline Styling/src/index.js
+++ ReactJs/286.Inline Styling/src/index.js
@@ -1,1 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+const csstyle = {
+  color: "blue",
+  fontSize: "10vh",
+  textAlign: "Center",
+};
+csstyle.color = "pink";
+root.render(<h1 style={csstyle}>Hello World!</h1>);
 
