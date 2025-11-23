dba5c56e62e1703051e2809402c7ab7dfa259daf
Azizul Islam <azizulislam5360@gmail.com>
2025-11-23 12:57:37 +0600

    Add basic React setup with copyright display

--- ReactJs/284. Jsx Practise /src/index.js
+++ ReactJs/284. Jsx Practise /src/index.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+const date = new Date();
+const year = date.getFullYear();
+root.render(
+  <div>
+    <p>Created By Azizul Islam</p>
+    <p>@Copyright {year}</p>
+  </div>
+);
 
