7f0191da454f749abeabd8618d345354bbed259c
Azizul Islam <azizulislam5360@gmail.com>
2025-12-02 18:29:50 +0600

    Add files via upload

--- ReactJs/298.Mapping components practise/src/components/App.jsx
+++ ReactJs/298.Mapping components practise/src/components/App.jsx
@@ -1,1 +1,27 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
 
+function create(contacts) {
+  return (
+    <Card
+      id={contacts.id}
+      key={contacts.id}
+      name={contacts.name}
+      img={contacts.imgURL}
+      tel={contacts.phone}
+      email={contacts.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(create)}
+    </div>
+  );
+}
+
+export default App;
