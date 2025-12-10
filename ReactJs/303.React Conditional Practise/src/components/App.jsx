831f1ff2ab2cdb6e897d2a585d83067d0b7f361c
Azizul Islam <azizulislam5360@gmail.com>
2025-12-10 23:08:24 +0600

    Add files via upload

--- ReactJs/303.React Conditional Practise/src/components/App.jsx
+++ ReactJs/303.React Conditional Practise/src/components/App.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import Form from "./Form";
 
+var userIsRegistered = false;
+
+function User() {}
+
+function App() {
+  return (
+    <div className="container">
+      <Form isReg={userIsRegistered} />
+    </div>
+  );
+}
+
+export default App;
