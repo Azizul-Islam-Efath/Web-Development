de34eb43e00a89bec397da09051d1b982b9bde87
Azizul Islam <azizulislam5360@gmail.com>
2025-12-15 23:33:58 +0600

    Add files via upload

--- ReactJs/306. UseState Hook Practise/src/components/App.jsx
+++ ReactJs/306. UseState Hook Practise/src/components/App.jsx
@@ -1,1 +1,20 @@
+import React, { useState } from "react";
 
+function App() {
+  setInterval(update, 1000);
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+
+  function update() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={update}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
